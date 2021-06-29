da765e105f1f550353abdb007df645685e9fd09e
Omar Munoz <49264189+omunoz-89@users.noreply.github.com>
2021-06-29 14:57:54 -0400

    Initial commit

---
+++ models/index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true
+});
+
+
+const db = mongoose.connection;
+
+//Setup event fro db to print connection
+db.once('open', () => {
+    console.log(`Connect to MongoDB at ${db.host}:${db.port}`);
+});
+
+db.on('error', err => console.log (`Database error`, err));
+
+//Import all models
+
+const User = require('./User');
+const Book = require('./Book');
+
+
+//Export all models from file
+module.exports = {
+    User,
+    Book,
+}
